fe42a63f91d2e1b7fddb92d576c9412aa8adbc0b
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2021-11-25 21:51:04 -0600

    Cambios
    Colocando comentarios

--- inv1 (3).m
+++ inv1 (3).m
@@ -2,7 +2,7 @@
 %School                :Tecnologico de Estudios Superiores de Jilotepec 
 %Specialty             :Ingeneria en Sistemas Computacionales 
 % Title                :Funcione inversa
-% Author               :FERNANDO ANGEL,CRISTIAN PADILLA,AMBAR ITZEL,MORA GARCIA,GABRIEL HERNANDEZ CUADRA,ANGELICA ARELLANO VILCHIZ
+% Author               : Fernando Angel Garcia,CRISTIAN PADILLA,AMBAR ITZEL,MORA GARCIA,GABRIEL HERNANDEZ CUADRA,ANGELICA ARELLANO VILCHIZ
 % Date                 :25/11/2021
 %Grupo                 :3101
 %Limpiamos variables
@@ -10,9 +10,9 @@ clear
 pkg load symbolic
 syms x
 x=(-2:0.1:5)%-2 es el valor de inicio y el 5 es el valor final y el del medio es como va avanzar de 0.1
-fx=(2.*x+3)./(5-x)
+fx=(2.*x+3)./(5-x)% función de f
 plot(x, fx)
-fy=(5.*x-3)./(x + 2)
+fy=(5.*x-3)./(x + 2)% función inversa 
 plot(x, fy,x, fx)
 grid on;
 title("FUNCION INVERSA");
